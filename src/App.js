e3829384d7c25eade213cbb89a7d4092197316a4
asennippan <aswin_senniappan@intuit.com>
2024-07-15 22:55:51 +0530

    Updated App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Let's start with react</p>
+        <p>By Aswin Senniappan</p>
       </header>
     </div>
   );
